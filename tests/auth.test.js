a21ce627edd83ccb98abd092cd2ad065adf351c1
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-09-11 12:24:54 +0700

    Revert "add chat"
    This reverts commit 6419ba219481896c06493bb48dc23d2597ab0397.

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -6,17 +6,17 @@ const {expect, should, assert} = require('chai')
 const { response } = require('../src/helpers/standardResponse')
 
 describe('Auth Login testing ', () => {
-  it('Wrong email format', (done) => {
-    supertest(APP_URL)
-    .post('/auth/login')
-    .send(`email=admin&password=123456`)
-    .expect(400)
-    .end((err, res) => {
-      expect(res.body.success).to.be.false
-      expect(res.body.message).equal('Wrong email format')
-      done()
-    })
-  })
+  // it('Wrong email format', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/login')
+  //   .send(`email=admin&password=123456`)
+  //   .expect(400)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.false
+  //     expect(res.body.message).equal('Wrong email format')
+  //     done()
+  //   })
+  // })
 
   it(`email not found`, (done) => {
     let req = {
@@ -91,54 +91,54 @@ describe('Auth Login testing ', () => {
     })
   })
 
-  it('Password length less than 6 characters ', (done) => {
-    supertest(APP_URL)
-    .post('/auth/login')
-    .send(`email=admin@email.com&password=1234`)
-    .expect(400)
-    .end((err, res) => {
-      expect(res.body.success).to.be.false
-      expect(res.body.message).equal('password length must be 6 characters at least')
-      done()
-    })
-  })
+  // it('Password length less than 6 characters ', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/login')
+  //   .send(`email=admin@email.com&password=1234`)
+  //   .expect(400)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.false
+  //     expect(res.body.message).equal('password length must be 6 characters at least')
+  //     done()
+  //   })
+  // })
 
-  it('Email not found ', (done) => {
-    supertest(APP_URL)
-    .post('/auth/login')
-    .send(`email=maj@email.com&password=123456`)
-    .expect(400)
-    .end((err, res) => {
-      expect(res.body.success).to.be.false
-      expect(res.body.message).equal('email not found')
-      done()
-    })
-  })
+  // it('Email not found ', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/login')
+  //   .send(`email=maj@email.com&password=123456`)
+  //   .expect(400)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.false
+  //     expect(res.body.message).equal('email not found')
+  //     done()
+  //   })
+  // })
 
-  it('wrong email and password', (done) => {
-    supertest(APP_URL)
-    .post('/auth/login')
-    .send(`email=reza@email.com&password=12345678`)
-    .expect(400)
-    .end((err, res) => {
-      expect(res.body.success).to.be.false
-      expect(res.body.message).equal('email or password is false')
-      done()
-    })
-  })
+  // it('wrong email and password', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/login')
+  //   .send(`email=reza@email.com&password=12345678`)
+  //   .expect(400)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.false
+  //     expect(res.body.message).equal('email or password is false')
+  //     done()
+  //   })
+  // })
 
-  it('success login', (done) => {
-    supertest(APP_URL)
-    .post('/auth/login')
-    .send(`email=reza@email.com&password=123456`)
-    .expect(200)
-    .end((err, res) => {
-      expect(res.body.success).to.be.true
-      expect(res.body.message).equal('Login success')
-      expect(res.body.results.token).to.be.a('string')
-      done()
-    })
-  })
+  // it('success login', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/login')
+  //   .send(`email=reza@email.com&password=123456`)
+  //   .expect(200)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.true
+  //     expect(res.body.message).equal('Login success')
+  //     expect(res.body.results.token).to.be.a('string')
+  //     done()
+  //   })
+  // })
 })
 
 describe('Auth Register Testing', () => {
@@ -219,65 +219,65 @@ describe('Auth Register Testing', () => {
   // })
 
 
-  it('Wrong email format', (done) => {
-    supertest(APP_URL)
-    .post('/auth/register')
-    .send(`email=admin&password=123456&phone_number=082212435142`)
-    .expect(400)
-    .end((err, res) => {
-      expect(res.body.success).to.be.false
-      expect(res.body.message).equal('Wrong email format')
-      done()
-    })
-  })
+  // it('Wrong email format', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/register')
+  //   .send(`email=admin&password=123456&phone_number=082212435142`)
+  //   .expect(400)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.false
+  //     expect(res.body.message).equal('Wrong email format')
+  //     done()
+  //   })
+  // })
 
-  it('Email is already in use ', (done) => {
-    supertest(APP_URL)
-    .post('/auth/register')
-    .send(`email=reza@email.com&password=123456&phone_number=082212435142`)
-    .expect(400)
-    .end((err, res) => {
-      expect(res.body.success).to.be.false
-      expect(res.body.message).equal('email is already in use')
-      done()
-    })
-  })
+  // it('Email is already in use ', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/register')
+  //   .send(`email=reza@email.com&password=123456&phone_number=082212435142`)
+  //   .expect(400)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.false
+  //     expect(res.body.message).equal('email is already in use')
+  //     done()
+  //   })
+  // })
 
-  it('Password length less than 6 characters ', (done) => {
-    supertest(APP_URL)
-    .post('/auth/register')
-    .send(`email=admin@email.com&password=1234&phone_number=082212435142`)
-    .expect(400)
-    .end((err, res) => {
-      expect(res.body.success).to.be.false
-      expect(res.body.message).equal('password length must be 6 characters at least')
-      done()
-    })
-  })
+  // it('Password length less than 6 characters ', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/register')
+  //   .send(`email=admin@email.com&password=1234&phone_number=082212435142`)
+  //   .expect(400)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.false
+  //     expect(res.body.message).equal('password length must be 6 characters at least')
+  //     done()
+  //   })
+  // })
 
-  it('Phone number length less than 11 characters ', (done) => {
-    supertest(APP_URL)
-    .post('/auth/register')
-    .send(`email=rezak@email.com&password=123456&phone_number=0822`)
-    .expect(400)
-    .end((err, res) => {
-      expect(res.body.success).to.be.false
-      expect(res.body.message).equal('phone number length must be 11 characters at least')
-      done()
-    })
-  })
+  // it('Phone number length less than 11 characters ', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/register')
+  //   .send(`email=rezak@email.com&password=123456&phone_number=0822`)
+  //   .expect(400)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.false
+  //     expect(res.body.message).equal('phone number length must be 11 characters at least')
+  //     done()
+  //   })
+  // })
 
-  it('Phone number is already in use ', (done) => {
-    supertest(APP_URL)
-    .post('/auth/register')
-    .send(`email=rezak@email.com&password=123456&phone_number=0877674635149`)
-    .expect(400)
-    .end((err, res) => {
-      expect(res.body.success).to.be.false
-      expect(res.body.message).equal('phone number is already in use')
-      done()
-    })
-  })
+  // it('Phone number is already in use ', (done) => {
+  //   supertest(APP_URL)
+  //   .post('/auth/register')
+  //   .send(`email=rezak@email.com&password=123456&phone_number=0877674635149`)
+  //   .expect(400)
+  //   .end((err, res) => {
+  //     expect(res.body.success).to.be.false
+  //     expect(res.body.message).equal('phone number is already in use')
+  //     done()
+  //   })
+  // })
 
   // it('success register', (done) => {
   //   supertest(APP_URL)

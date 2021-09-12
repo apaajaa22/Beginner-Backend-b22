15ca12f8471cb4ac4f7d625735b833de034f74da
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-09-12 17:47:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://fm22rezacoffeeshop.herokuapp.com'
+    origin: 'https://fm22rezacoffeeshop.herokuapp.com/'
   }
 })
 
